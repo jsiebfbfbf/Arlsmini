02d486315b5cfaf5c8343b108fbc7b0849565bcf
jsiebfbfbf <ishanmadusanke897@gmail.com>
2026-06-17 00:42:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,8 +9,8 @@ module.exports = {
     // ===========================================================
     // 1. CONFIGURATION DE BASE (Session & Database)
     // ===========================================================
-    SESSION_ID: process.env.SESSION_ID || "KAVUUA", 
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://offarslan_db_user:arslanmd@cluster0.xrqkzwg.mongodb.net/?appName=Cluster0',
+    SESSION_ID: process.env.SESSION_ID || "KAVUA", 
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://dtz_dula:dtz_nova_x_md@cluster0.evuexjr.mongodb.net/?appName=Cluster0',
     
     // ===========================================================
     // 2. INFORMATIONS DU BOT
